1688d5f689dbd6988690e7ddd5200001d2a91616
Sawako Ishida <sawako.yoriko@gmail.com>
2015-11-10 15:06:39 -0800

    added event listener

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ function PhotoObject(photoName, photoPath) {
   this.photoName = photoName;
   this.photoPath = photoPath;
   this.numOfVotes = 0;
+
 }
 
 //New Photo Objects//
@@ -20,23 +21,77 @@ var img9 = new PhotoObject("lionArt","img/artpic9.jpg");
 var img10 = new PhotoObject("pikachu","img/artpic10.jpg");
 var img11 = new PhotoObject("cat","img/artpic11.jpg");
 var img12 = new PhotoObject("clock","img/artpic12.jpg");
-console.log(img1);
+// console.log(img1);
 
 //Pushing img new objects into array called photoGallary//
 photoGallary.push(img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12);
-console.log(photoGallary);
+// console.log(photoGallary);
 
-//Displaying onto browser//
-var randomPhoto = Math.ceil(Math.random()*photoGallary.length);
-console.log(randomPhoto);
-var imgFrame1 = document.createElement("img");
-    document.getElementById("left-img").appendChild(imgFrame1);
+//Calculatng random//
+calcRandom = function (){
+  return Math.floor(Math.random()*photoGallary.length);
+  console.log(randomPhoto);
+}
 
-var imgFrame2 = document.createElement("img");
-    document.getElementById("right-img").appendChild(imgFrame2);
+var imgElement1;
+var imgElement2;
+display = function (){
+  var frame1 = photoGallary[calcRandom()].photoPath;//make a lil mod when event listner
+  var frame2 = photoGallary[calcRandom()].photoPath;
+  while(frame1 === frame2){
+    frame2 = photoGallary[calcRandom()].photoPath;
+  }
+  imgElement1 = document.createElement("img");
+  imgElement2 = document.createElement("img");
+  imgElement1.src = frame1;
+  imgElement1.id = "img1"
+  imgElement2.src = frame2;
+  imgElement2.id = "img2"
+  document.getElementById("left-img").appendChild(imgElement1);
+  document.getElementById("right-img").appendChild(imgElement2);
+}
+
+display();
 
 
 
+
+imgElement1.addEventListener('click',function () {
+  this.src = photoGallary[calcRandom()].photoPath;
+  imgElement2.src = photoGallary[calcRandom()].photoPath;
+    while(this.src === imgElement2.src){
+    imgElement2.src = photoGallary[calcRandom()].photoPath;
+  }
+})
+
+imgElement2.addEventListener('click',function () {
+  this.src = photoGallary[calcRandom()].photoPath;
+  imgElement1.src = photoGallary[calcRandom()].photoPath;
+    while(this.src === imgElement1.src){
+    imgElement2.src = photoGallary[calcRandom()].photoPath;
+  }
+})
+// display.imgElement2.addEventListener('click',display(),false);
+
+// var imgElement1 = document.createElement("img")
+    // document.getElementById("left-img").appendChild(imgElement1);
+    // imgElement1.setAttribute("width", "200px");
+    // imgElement1.setAttribute("height", "200px");
+
+
+
+
+//missing step-and src with array inside to attach to <div> i have using setAttribute()
+// var imgElement2 = document.createElement("img");
+//     document.getElementById("right-img").appendChild(imgElement2);
+    // imgElement2.setAttribute("src", );
+    // imgElement2.setAttribute("width", "200px");
+    // imgElement2.setAttribute("height", "200px");
+    // imgElement2.src = photoGallary.photoPath;
+
+
+
+//eventlistener with a click
 
 
 //11.9- Assignement Adding 2 images onto a browser
