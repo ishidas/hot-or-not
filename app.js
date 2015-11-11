3fac2de15da574479278102ab781fdc79fc99ca2
Sawako Ishida <sawako.yoriko@gmail.com>
2015-11-11 15:57:24 -0800

    chart works and updates data as I click pic

--- app.js
+++ app.js
@@ -63,9 +63,11 @@ display2 = function (){
 
 
 var selectionLeft = function () {
-  photoGallary[randNum1].numOfVotes++ ;
-  console.log(photoGallary[randNum1].numOfVotes + 1);
-  console.log(photoGallary[randNum1].photoName + " has " + photoGallary[randNum1].numOfVotes + " votes");
+  photoGallary[randNum1].numOfVotes += 1 ;
+  myBarChart.datasets[0].bars[randNum1].value = photoGallary[randNum1].numOfVotes
+  myBarChart.update();
+  console.log(photoGallary[randNum1]);
+  // console.log(photoGallary[randNum1].photoName + " has " + photoGallary[randNum1].numOfVotes + " votes");
   randNum1 = calcRandom();
   while(randNum1 === randNum2) {
     randNum1 = calcRandom();
@@ -74,14 +76,19 @@ var selectionLeft = function () {
   imgElement1.src = path1;
   console.log(path1);
 
+
+
 }
 
   // imgElement1.parentNode.removeChild(selection1);
 
 var selectionRight = function () {
-  photoGallary[randNum2].numOfVotes++ ;
-  console.log(photoGallary[randNum2].numOfVotes + 1);
-  console.log(photoGallary[randNum2].photoName + " has " + photoGallary[randNum2].numOfVotes + " votes");
+  photoGallary[randNum2].numOfVotes += 1 ;
+  myBarChart.datasets[0].bars[randNum2].value = photoGallary[randNum2].numOfVotes
+  myBarChart.update();
+
+  console.log(photoGallary[randNum2]);
+  // console.log(photoGallary[randNum2].photoName + " has " + photoGallary[randNum2].numOfVotes + " votes");
   randNum2 = calcRandom();
   while(randNum1 === randNum2) {
     randNum2 = calcRandom();
@@ -93,8 +100,7 @@ var selectionRight = function () {
 display1();
 display2();
 
-imgElement1.addEventListener('click',selectionLeft);
-imgElement2.addEventListener('click',selectionRight);
+
 
 // Bar Chart using Chart.js//
 var ctx = document.getElementById("myChart").getContext("2d");
@@ -103,110 +109,26 @@ var ctx = document.getElementById("myChart").getContext("2d");
 // function createChart() {
 
 var data = {
-  "labels":["lion","hearts","flowers","elephant","space", "hawks", "cuteElephant","snoopy","pikachu","simpsons","cat","clock"],
-  "datasets":[
+  labels:["lion","hearts","flowers","elephant","space", "hawks", "cuteElephant","snoopy","pikachu","simpsons","cat","clock"],
+  datasets:[
       {
-        label: "Votes Tracker 0",
+        label: "Votes Tracker",
         fillColor: "rgba(220,220,220,0.5)",
         strokeColor: "rgba(220,220,220,0.8)",
         highlightFill: "rgba(220,220,220,0.75)",
         highlightStroke: "rgba(220,220,220,1)",
-        data:[photoGallary[0].numOfVotes]
+        data:[0,0,0,0,0,0,0,0,0,0,0]
       },
-      {
-        label: "Votes Tracker 1",
-        fillColor: "rgba(151,187,205,0.5)",
-        strokeColor: "rgba(151,187,205,0.8)",
-        highlightFill: "rgba(151,187,205,0.75)",
-        highlightStroke: "rgba(151,187,205,1)",
-        data:[photoGallary[1].numOfVotes]
-      },
-      {
-        label: "Votes Tracker 2",
-        fillColor: "rgba(220,220,220,0.5)",
-        strokeColor: "rgba(220,220,220,0.8)",
-        highlightFill: "rgba(220,220,220,0.75)",
-        highlightStroke: "rgba(220,220,220,1)",
-        data:[photoGallary[2].numOfVotes]
-      },
-      {
-        label: "Votes Tracker 3",
-        fillColor: "rgba(151,187,205,0.5)",
-        strokeColor: "rgba(151,187,205,0.8)",
-        highlightFill: "rgba(151,187,205,0.75)",
-        highlightStroke: "rgba(151,187,205,1)",
-        data:[photoGallary[3].numOfVotes]
-      },
-      {
-        label: "Votes Tracker 4",
-        fillColor: "rgba(220,220,220,0.5)",
-        strokeColor: "rgba(220,220,220,0.8)",
-        highlightFill: "rgba(220,220,220,0.75)",
-        highlightStroke: "rgba(220,220,220,1)",
-        data:[photoGallary[4].numOfVotes]
-      },
-      {
-        label: "Votes Tracker 5",
-        fillColor: "rgba(151,187,205,0.5)",
-        strokeColor: "rgba(151,187,205,0.8)",
-        highlightFill: "rgba(151,187,205,0.75)",
-        highlightStroke: "rgba(151,187,205,1)",
-        data:[photoGallary[5].numOfVotes]
-      },
-      {
-        label: "Votes Tracker 6",
-        fillColor: "rgba(220,220,220,0.5)",
-        strokeColor: "rgba(220,220,220,0.8)",
-        highlightFill: "rgba(220,220,220,0.75)",
-        highlightStroke: "rgba(220,220,220,1)",
-        data:[photoGallary[6].numOfVotes]
-      },
-      {
-        label: "Votes Tracker 7",
-        fillColor: "rgba(151,187,205,0.5)",
-        strokeColor: "rgba(151,187,205,0.8)",
-        highlightFill: "rgba(151,187,205,0.75)",
-        highlightStroke: "rgba(151,187,205,1)",
-        data:[photoGallary[7].numOfVotes]
-      },
-      {
-        label: "Votes Tracker 8",
-        fillColor: "rgba(220,220,220,0.5)",
-        strokeColor: "rgba(220,220,220,0.8)",
-        highlightFill: "rgba(220,220,220,0.75)",
-        highlightStroke: "rgba(220,220,220,1)",
-        data:[photoGallary[8].numOfVotes]
-      },
-      {
-        label: "Votes Tracker 9",
-        fillColor: "rgba(151,187,205,0.5)",
-        strokeColor: "rgba(151,187,205,0.8)",
-        highlightFill: "rgba(151,187,205,0.75)",
-        highlightStroke: "rgba(151,187,205,1)",
-        data:[photoGallary[9].numOfVotes]
-      },
-      {
-        label: "Votes Tracker 10",
-        fillColor: "rgba(220,220,220,0.5)",
-        strokeColor: "rgba(220,220,220,0.8)",
-        highlightFill: "rgba(220,220,220,0.75)",
-        highlightStroke: "rgba(220,220,220,1)",
-        data:[photoGallary[10].numOfVotes]
-      },
-      {
-        label: "Votes Tracker 11",
-        fillColor: "rgba(151,187,205,0.5)",
-        strokeColor: "rgba(151,187,205,0.8)",
-        highlightFill: "rgba(151,187,205,0.75)",
-        highlightStroke: "rgba(151,187,205,1)",
-        data:[photoGallary[11].numOfVotes]
-      },
-
     ]
    }
 var myBarChart = new Chart(ctx).Bar(data);
 
-// createChart();
+// myBarChart = datasets[1].bars[1].value = photoGallary[].numOfVotes;
+
+
+imgElement1.addEventListener('click',selectionLeft);
+imgElement2.addEventListener('click',selectionRight);
+
 
 
 
