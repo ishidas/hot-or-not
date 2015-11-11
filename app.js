cc4138c4a2e12e3930c7e3e6e88b0a7d166d7468
Sawako Ishida <sawako.yoriko@gmail.com>
2015-11-11 10:58:50 -0800

    pics changes and add counts on left one

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function PhotoObject(photoName, photoPath) {
 }
 
 //New Photo Objects//
-var img1 = new PhotoObject("lionArt","img/artpic1.jpg");
+var img1 = new PhotoObject("lion","img/artpic1.jpg");
 var img2 = new PhotoObject("hearts","img/artpic2.jpg");
 var img3 = new PhotoObject("flowers","img/artpic3.jpg");
 var img4 = new PhotoObject("elephant","img/artpic4.jpg");
@@ -17,8 +17,8 @@ var img5 = new PhotoObject("space","img/artpic5.jpg");
 var img6 = new PhotoObject("hawks","img/artpic6.jpg");
 var img7 = new PhotoObject("cuteElephant","img/artpic7.jpg");
 var img8 = new PhotoObject("snoopy","img/artpic8.jpg");
-var img9 = new PhotoObject("lionArt","img/artpic9.jpg");
-var img10 = new PhotoObject("pikachu","img/artpic10.jpg");
+var img9 = new PhotoObject("pikachu","img/artpic9.jpg");
+var img10 = new PhotoObject("simpsons","img/artpic10.jpg");
 var img11 = new PhotoObject("cat","img/artpic11.jpg");
 var img12 = new PhotoObject("clock","img/artpic12.jpg");
 // console.log(img1);
@@ -40,49 +40,59 @@ var frame2;
 var randNum1 = calcRandom();
 var randNum2 = calcRandom();
 
-display = function (){
+display1 = function (){
   frame1 = photoGallary[randNum1].photoPath;//make a lil mod when event listner
   console.log(frame1);
-  frame2 = photoGallary[randNum2].photoPath;
-  // while(frame1 === frame2){
-  //   frame2 = photoGallary[calcRandom()].photoPath;
-  // }
   imgElement1 = document.createElement("img");
-  imgElement2 = document.createElement("img");
   imgElement1.src = frame1;
   imgElement1.id = "img1"
+  document.getElementById("left-img").appendChild(imgElement1);
+}
+
+display2 = function (){
+     while(randNum1 === randNum2) {
+      randNum2 = calcRandom();
+  }
+  frame2 = photoGallary[randNum2].photoPath;
+  imgElement2 = document.createElement("img");
   imgElement2.src = frame2;
   imgElement2.id = "img2"
-  document.getElementById("left-img").appendChild(imgElement1);
   document.getElementById("right-img").appendChild(imgElement2);
-  }
+}
 
 
-  var selection1 = function () {
-  this.src = frame1;
-  imgElement2.src = frame2;
+var selectionLeft = function () {
+  console.log(path1);
   photoGallary[randNum1].numOfVotes++ ;
   console.log(photoGallary[randNum1].numOfVotes + 1);
   console.log(photoGallary[randNum1].photoName + " has " + photoGallary[randNum1].numOfVotes + " votes");
-    while(this.src === imgElement2.src){
-    imgElement2.src = frame2;
-    }
-  // imgElement1.parentNode.removeChild(selection1);
-  display();
+  randNum1 = calcRandom();
+
+  var path1 = photoGallary[randNum1].photoPath;
+  imgElement1.src = path1;
+
 }
 
-//   var selection2 = function () {
+  // imgElement1.parentNode.removeChild(selection1);
+
+  var selectionRight = function () {
 //   this.src = frame2;
 //   imgElement1.src = frame1;
 //     while(this.src === imgElement1.src){
 //     imgElement2.src = frame2;
 //   }
-// }
+photoGallary[randNum2].numOfVotes++ ;
+  console.log(photoGallary[randNum2].numOfVotes + 1);
+  console.log(photoGallary[randNum2].photoName + " has " + photoGallary[randNum2].numOfVotes + " votes");
 
-display();
+}
 
+display1();
+display2();
+// .parentNode.removeChild(display1());
 
-imgElement1.addEventListener('click',selection1);
-// imgElement2.addEventListener('click',selection2);
+imgElement1.addEventListener('click',selectionLeft);
+
+imgElement2.addEventListener('click',selectionRight);
 
 
