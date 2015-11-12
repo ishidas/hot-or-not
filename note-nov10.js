fd89e945f3b1f6500d8b45cc64c08aca08e7e069
Sawako Ishida <sawako.yoriko@gmail.com>
2015-11-12 11:05:49 -0800

    updated css

--- note-nov10.js
+++ note-nov10.js
@@ -1,3 +1,36 @@
+//Nov 12
+stateVoting{
+  two new random numbers ()
+  display the phortos ()
+  hide the chart? ()
+  turn off or hide the '2 more photos' button ()
+}
+stateResult() {
+  tally votes ()
+  render the Charts(local storage) ()
+  (local storage) ()
+  turn off the event listeners on the pitures to prevent further voting ()
+  turn on or show the '2 more photos button' ()
+}
+
+buttonTwoNewPhotos.addEventListener('click', stateResult);
+
+picLeft.addEventListener('click', stateResult);
+picRight.addEventListener('click', stateResult);
+
+*****************************************
+
+Another way to manage state
+
+if (voting) {
+  thing 1
+  thing 2
+} else {
+  other thing 1
+  other thing 2
+}
+
+
 //accumulating votes:
 Resizing image -> just crop it in a photo editor
 
